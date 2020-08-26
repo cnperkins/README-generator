78fdb05002a6f45d233b055040e69bc4e2e55167
chlokins <cnpolansky@gmail.com>
2020-08-26 18:43:18 -0500

    code working still need myreadme

---
+++ Develop/README2.md
@@ -0,0 +1,34 @@
+ 
+    # rdh
+      
+        ## Github Username: cnperkins
+       
+        ## Email cnpolansky@gmail.com
+      
+        ## Description: coder
+      
+      
+        ## Table of Contents: 
+        * [Description] (##Description)
+        * [Installation] (##Installation)
+        * [Usage] (###Usage)
+        * [Licenses] (###Licenses)
+        * [Contributing] (###Contributing)
+        * [Tests] (###Tests)
+      
+      
+        ### Installation: none
+      
+      
+        ### Usage: create
+      
+      
+        ### Licenses: no
+      
+      
+        ### Contributing: 2
+      
+      
+        ### Tests: 3
+      
+       
--- Develop/index.js
+++ Develop/index.js
@@ -1,89 +1,109 @@
-var inquirer = require("inquirer");
-var fs = require('fs');
-const generateMarkdown = require(`./utils/generateMarkdown.js`);
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // array of questions for user
-inquirer.prompt([{
-    type: "input",
-    name: "title",
-    message: "What is your title?"
-},
-{
-    type: "input",
-    name: "description",
-    message: "What is your description?"
-},
-{
-    type: "input",
-    name: "table",
-    message: "What is the table of contents?"
-},
-{
-    type: "input",
-    name: "installation",
-    message: "What is installation?"
-},
-{
-    type: "input",
-    name: "usage",
-    message: "What is the usage?"
-},
-{
-    type: "list",
-    message: "What licenses are needed?",
-    name: "license",
-    choices: [
-        "email",
-        "phone",
-        "telekinesis"
-    ]
-},
-{
-    type: "input",
-    name: "contributing",
-    message: "What is contiubuting?"
-},
-{
-    type: "input",
-    name: "tests",
-    message: "What tests are used?"
-},
-{
-    type: "input",
-    name: "email",
-    message: "What is your email?"
-},
-{
-    type: "input",
-    name: "github",
-    message: "What is your github username?"
+function promptUser() {
+    return inquirer.prompt([{
+        type: "input",
+        name: "title",
+        message: "What is your title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is your description?"
+    },
+
+    {
+        type: "input",
+        name: "installation",
+        message: "What installations are needed?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What is the usage?"
+    },
+    {
+        type: "input",
+        message: "Are licenses needed?",
+        name: "license",
+
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Contributions?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "How many tests are used?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your github username?"
+    }
+
+    ])
 }
+function generateREADME2(data) {
 
-])
 
-// function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(README2.md, JSON.stringify(data, null, '\t'), function (err) {
+    return ` 
+    # ${data.title}
+      
+        ## Github Username: ${data.github}
+       
+        ## Email ${data.email}
+      
+        ## Description: ${data.description}
+      
+      
+        ## Table of Contents: 
+        * [Description] (##Description)
+        * [Installation] (##Installation)
+        * [Usage] (###Usage)
+        * [Licenses] (###Licenses)
+        * [Contributing] (###Contributing)
+        * [Tests] (###Tests)
+      
+      
+        ### Installation: ${data.installation}
+      
+      
+        ### Usage: ${data.usage}
+      
+      
+        ### Licenses: ${data.license}
+      
+      
+        ### Contributing: ${data.contributing}
+      
+      
+        ### Tests: ${data.tests}
+      
+       `;
 
-        if (err) {
-            return console.log(err);
-        }
+}
+promptUser()
+    .then(function (data) {
+        const md = generateREADME2(data);
+        return writeFileAsync("README2.md", md);
+    })
+    .then(function () {
 
-        console.log("Success!");
-
+        console.log("Successfully wrote to README2.md");
+    })
+    .catch(function (err) {
+        console.log(err);
     });
-}
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
-
-
-
-
-
